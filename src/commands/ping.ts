063c8e1b505ed80b9a98dc1e6e8566ddd86e2188
ethereal-chxn <ethchan@ucdavis.edu>
2025-08-13 16:04:46 -0700

    Set up linter and modified ping command to send the bot's latency

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,9 +1,10 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("a test command.");
+  .setName("ping")
+  .setDescription("a test command.");
 
 export async function execute(interaction: CommandInteraction) {
-    return interaction.reply("Pong!");
+  const ping: number = Date.now() - interaction.createdTimestamp;
+  return interaction.reply(`Pong! Latency: ${ping}`);
 }
